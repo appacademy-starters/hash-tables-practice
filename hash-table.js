67ca726c4ac946551d7c2db219145cefdfc7fc57
Brady Fukumoto <bradyfukumoto@gmail.com>
2021-04-01 17:49:15 -0700

    hash table practice starter

---
+++ hash-table.js
@@ -0,0 +1,31 @@
+// Do not change this
+class KeyValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class HashTable {
+
+  constructor(numBuckets = 4) {
+    // fill this in
+  }
+
+  hash(key) {
+    // fill this in
+  }
+
+  hashMod(key) {
+    // fill this in
+  }
+
+  insert(key, value) {
+    // fill this in
+  }
+
+}
+
+
+module.exports = HashTable;
